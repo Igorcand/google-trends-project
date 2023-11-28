dda3ddeb7d7d626a4be3ea0535c4fc610b3a8154
Igor Candido <igorcr39@gmail.com>
2023-11-28 20:05:55 -0300

    get and read google trends

--- main.go
+++ main.go
@@ -9,35 +9,51 @@ import (
 )
 
 type RSS struct{
-	XMLName		xml.Name
-	Channel 	*Channel
+	XMLName		xml.Name 		`xml: "rss"`
+	Channel 	*Channel		`xml: "channel"`
 }
 
 type Channel struct{
-	Title		string 
-	ItemList	[]Item
+	Title		string 			`xml: "title"`
+	ItemList	[]Item			`xml: "item"`
 }
 
 type Item struct {
-	Title 		string
-	Link 		string 
-	Traffic 	string
-	NewsItems 	[]News 
+	Title 		string			`xml: "title"`
+	Link 		string 			`xml: "link"`
+	Traffic 	string  		`xml: "approx_traffic"`
+	NewsItems 	[]News 			`xml: "new_item"`
 }
 
 type News struct{
-	Headline 		string
-	Headlinelink 	string
+	Headline 		string		`xml: "new_item_title"`
+	Headlinelink 	string		`xml: "new_item_url"`
 }
 
 func main(){
-	readGoogleTrends
+	var r RSS 
+
+	data := readGoogleTrends()
 }
 
-func readGoogleTrends(){
-	getGoogleTrends
+func getGoogleTrends() *http.Response {
+	resp, err := http.Get("https://trends.google.com.br/trends/trendingsearches/daily/rss?geo=US")
+
+	if err != nil{
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	return resp
 }
 
-func getGoogleTrends(){
+func readGoogleTrends() []byte {
+	resp := getGoogleTrends()
+	data, err := ioutil.ReadAll(resp.Body)
 
+	if err != nil{
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	return data
 }

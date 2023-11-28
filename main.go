f79913415c1e0fbb74a46406d17daaad1de273a3
Igor Candido <igorcr39@gmail.com>
2023-11-28 19:43:14 -0300

    create structs

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"net/hhtp"
+	"os"
+)
+
+type RSS struct{
+	XMLName		xml.Name
+	Channel 	*Channel
+}
+
+type Channel struct{
+	Title		string 
+	ItemList	[]Item
+}
+
+type Item struct {
+	Title 		string
+	Link 		string 
+	Traffic 	string
+	NewsItems 	[]News 
+}
+
+type News struct{
+	Headline 		string
+	Headlinelink 	string
+}
+
+func main(){
+	readGoogleTrends
+}
+
+func readGoogleTrends(){
+	getGoogleTrends
+}
+
+func getGoogleTrends(){
+
+}
